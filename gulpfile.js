39059ef6afe8743ad2cd4d612f615e559d6e50ff
Arthur Guiot <arguiot@gmail.com>
2017-04-15 17:53:01 +1000

    Added new functions !😏
    Added scrollToTop, scrollToBottom, scrollRelative

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,14 @@
 var gulp = require('gulp'),
     rename = require('gulp-rename')
     uglify = require('gulp-uglify');
-gulp.task('default', function() {
-  gulp.src('src/*.js')
+gulp.task("minify", function () {
+	gulp.src('src/*.js')
     .pipe(uglify())
     .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('dist'))
 });
+gulp.task("copy", function () {
+	gulp.src('src/*.js')
+    .pipe(gulp.dest('dist'))
+});
+gulp.task('default', [ 'minify', 'copy' ]);
